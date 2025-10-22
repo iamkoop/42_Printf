6742a78089c3c8222a18bc70c97d12e3b1a02214
Nils Druon <nildruon@c3r8p3.42vienna.com>
2025-10-22 17:53:00 +0200

    printf works now if argcount is 0

--- ft_printf.c
+++ ft_printf.c
@@ -6,46 +6,52 @@
 /*   By: nildruon <nildruon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 21:07:04 by nildruon          #+#    #+#             */
-/*   Updated: 2025/10/22 14:54:22 by nildruon         ###   ########.fr       */
+/*   Updated: 2025/10/22 17:52:07 by nildruon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 #include <stdio.h>
-//todo : check out printf for the various weird interactions with the % being alone and stuff
-
-static int arg_count (const char * format)
+//todo : check out printf for the various weird 
+//			interactions with the % being alone and stuff
+static int	arg_count(const char *format)
 {
 	int		i;
 	int		cnt;
 	char	*conversion_specifier;
 	char	*set;
+	int		found_percent_sign;
 
 	i = 0;
 	cnt = 0;
+	found_percent_sign = 0;
 	set = "cspdiuxX%";
 	while (format[i])
 	{
-		if(format[i] == '%')
+		found_percent_sign = 0;
+		if (format[i] == '%')
+		{
+			found_percent_sign = 1;
 			i++;
-		while(format[i] == ' ')
+		}
+		while (format[i] == ' ')
 			i++;
-		if(ft_strchr(set,format[i]) != NULL)
+		if (ft_strchr(set, format[i]) != NULL && found_percent_sign)
 			cnt++;
 		i++;
 	}
 	return (cnt);
 }
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-	int count_var;
+	int		count_var;
+	va_list	variable_arg;
 
 	count_var = arg_count(format);
-	printf("%zu",ft_strlen(format));
-	if(ft_strlen(format) == 0)
+	if (ft_strlen(format) == 0)
 		return (0);
-	if(arg_count(format) == 0)
+	if (arg_count(format) == 0)
 	{
 		ft_putstr_fd((char *)format, 1);
 		return (ft_strlen(format));
@@ -53,12 +59,7 @@ int ft_printf(const char *format, ...)
 	return (0);
 }
 
-int main(void)
+int	main(void)
 {
 	ft_printf("Hello World");
 }
-/* int main(void)
-{
-	int test = 120000;
-	printf("%d",test);	
-} */
