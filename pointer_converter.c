5ec416c424061740ee6aee19dcd4fa8f8516d682
Nils Druon <personal_laptop_ndruon@>
2025-11-03 03:08:41 +0100

    newer version

--- pointer_converter.c
+++ pointer_converter.c
@@ -6,7 +6,7 @@
 /*   By: nilsdruon <nilsdruon@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 21:01:06 by nildruon          #+#    #+#             */
-/*   Updated: 2025/11/03 02:16:41 by nilsdruon        ###   ########.fr       */
+/*   Updated: 2025/11/03 03:07:54 by nilsdruon        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ int	print_pointer(void *ptr)
 	nb = (uintptr_t)ptr;
 	ft_putstr_fd("0x", 1);
 	len = 2 + print_hex('x', nb, 0);
-	print_hex('x', nb, 0);
+	//print_hex('x', nb, 0);
 	return(len);
 }
